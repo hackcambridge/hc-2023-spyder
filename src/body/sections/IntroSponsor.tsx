fee8d05144233acbfe15b12153f32256fab0351c
Pragash-Mohanarajah <mpragash@icloud.com>
2023-02-16 18:54:14 +0000

    HC Spyder Logo and Embedded Reply Video

--- src/body/sections/IntroSponsor.tsx
+++ src/body/sections/IntroSponsor.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import "./IntroSponsor.scss";
 import Fade from "react-reveal";
+import Logo from '../components/Logo';
 import IntroSponsorLogo from "../components/IntroSponsorLogo";
+import logoPath from "../../shared/img/logo.svg";
 import ReplyLogo from "../../shared/img/sponsors/reply.png";
 
 export type IntroSponsorProps = {};
@@ -12,6 +14,7 @@ export default function IntroSponsor(props: IntroSponsorProps) {
       <div className="intro-sponsor-col intro-sponsor-col-1">
         <Fade left>
           <h1>Sponsored and Cohosted with Reply</h1>
+          <Logo logo={logoPath}/>
           <IntroSponsorLogo introsponsorlogo={ReplyLogo} />
         </Fade>
         <Fade bottom>
@@ -24,6 +27,10 @@ export default function IntroSponsor(props: IntroSponsorProps) {
               by the new technological and communication paradigms such as Artificial Intelligence, Big Data, <br></br>
               Cloud Computing, Digital Communication, the Internet of Things and Mobile and Social Networking. <br></br>
               In so doing, it aims to optimise and integrate processes, applications and devices.
+              <br></br>
+               
+              <br></br>
+              <iframe title="Reply Video" height="500" width="1100" src="https://d1z7icpqkuzo43.cloudfront.net/production/28/92/ce4eb3d9-80e9-4e34-8da3-f3405489978d/9e6633f4-8d84-4c6c-9b2f-a47b45cc1618.mp4"></iframe>
               </p>
             </div>
           </div>
